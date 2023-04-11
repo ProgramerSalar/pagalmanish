c85b06992a6ebb988296926c65a6fec17061817a
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-11 13:56:31 +0530

    project Layout

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('' , views.index , name = 'index')
+    path('' , views.index , name = 'index'),
+    path('about/' , views.about , name = 'about'),
+    path('contact/' , views.contact , name= 'contact'),
+    path('tracker/' , views.tracker , name='tracker'),
+    path('search/' , views.search , name='search'),
+    path('checkout/' , views.checkout , name='checkout')
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,4 +5,25 @@ from django.http import HttpResponse
 def index(request):
     return render(request , 'shop/index.html')
 
-    
+def about(request):
+    return HttpResponse('about page')
+
+
+def contact(request):
+    return HttpResponse('contact page')
+
+
+def tracker(request):
+    return HttpResponse('tracker  page')
+
+def search(request):
+    return HttpResponse('search page')
+
+
+def productview(request):
+    return HttpResponse('product view page')
+
+
+
+def checkout(request):
+    return HttpResponse('checkout page')
