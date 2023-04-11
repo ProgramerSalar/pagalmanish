303c616d5cdeb238d6cb9fc4f9c0143690e15c0c
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-11 22:11:41 +0530

    showing multiple product slides

--- shop/views.py
+++ shop/views.py
@@ -5,10 +5,15 @@ from math import ceil
 
 # Create your views here.
 def index(request):
-    products  = Product.objects.all()
-    n = len(products)
-    nnSlides = n//4 + ceil((n/4) + (n//4))
-    params = {'no_of_slides': nnSlides , 'range': range(1, nnSlides) , 'product': products}
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(prod)
+        nSlides = n//4 + ceil((n/4) - (n//4))
+        allProds.append(([prod , range(1 , nSlides) , nSlides]))
+        params = {'allProds': allProds}
     return render(request , 'shop/index.html', params)
 
 def about(request):
